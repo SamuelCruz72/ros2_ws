c231e8ce71c576c6e5514f197942f5f2f9003c56
SamuelCruz72 <johndoe@example.com>
2025-04-23 21:13:23 -0500

    cambio directorios

---
+++ lab_1/ros2_ws/turtlebot3_ws/build/dynamixel_sdk/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/samuelcruz/Laboratorio_Robotica/lab_1/ros2_ws/turtlebot3_ws/src/DynamixelSDK/ros/dynamixel_sdk
+# Build directory: /home/samuelcruz/Laboratorio_Robotica/lab_1/ros2_ws/turtlebot3_ws/build/dynamixel_sdk
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
