fc2d4f7afecf4b1a111b27aeac5a72f43a3a7af9
SamuelCruz72 <johndoe@example.com>
2025-07-02 12:06:39 -0500

    phantom_ws

---
+++ ros2_ws/phantom_ws/build/phantomx_pincher_demos/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/samuelcruz/Lab_Robotica/ros2_ws/phantom_ws/install/phantomx_pincher_description/share/phantomx_pincher_description/package.sh"
+. "/home/samuelcruz/Lab_Robotica/ros2_ws/phantom_ws/install/phantomx_pincher_moveit_config/share/phantomx_pincher_moveit_config/package.sh"
+. "/home/samuelcruz/Lab_Robotica/ros2_ws/phantom_ws/install/phantomx_pincher/share/phantomx_pincher/package.sh"
